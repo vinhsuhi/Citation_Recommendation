fbc23a7edf171d5a89ff7b9716f4e134d5b19160
vinhsuhi <vinhbachkhoait@gmail.com>
2018-08-01 21:02:01 +0700

    full extract raw data

--- data_utils/extract_raw_data.py
+++ data_utils/extract_raw_data.py
@@ -1,10 +1,10 @@
 import json
-from library.utils import MongoDB, Pickle # Use this if you fun file as Vinh,
-# from utils import MongoDB, Pickle       # or use this if you prefer Thu's way.
+from library.utils import MongoDB, Pickle
 import numpy as np
 import os
 from copy import deepcopy
 import random
+import time
 def read_file(path):
 	"""
 	read file s2-corpus-00
@@ -74,12 +74,13 @@ def read_file(path):
 	"""
 	# if os.path.exists('processed_data/abstract.json'):
 		# return 0
+	papers = MongoDB.get_collection(database_name='OpenCorpus', collection_name='papers')
+	n_papers = papers.count()
+	if n_papers > 300000:
+		print("Data has already in DB, number of papers is: ", n_papers) # 26673
+		return papers
+
 	with open(path, 'r', encoding='utf-8', errors='ignore') as f:
-		papers = MongoDB.get_collection(database_name='OpenCorpus', collection_name='papers')
-		n_papers = papers.count()
-		# if n_papers > 30000:
-			# print("Data has already in DB, number of papers is: ", n_papers) # 26673
-			# return papers
 		count = n_papers
 		for i, line in enumerate(f):
 			paper_info = json.loads(line)
@@ -116,13 +117,11 @@ def save_pkl(papers):
 			just consider inside our collection. Papers have both empty incitations and outcitations will be mark as isolate
 			and will be remove from collection.
 	"""
-	maps = MongoDB.get_collection(database_name='OpenCorpus', collection_name='cite_map')
-	# if maps.count() > 1000:
-		# print("data have been already in DB", maps.count())
-		# return maps
-	# if os.path.exists('processed_data/ids.pkl'):
-		# ids = Pickle.load_obj('processed_data/ids')
-	# else:
+	cite_map = MongoDB.get_collection(database_name='OpenCorpus', collection_name='cite_map')
+	n_cite = cite_map.count()
+	if n_cite > 10000:
+		print("Data has already in DB, number of cite is: ", n_cite)
+		return cite_map
 	ids = []
 	cursor = papers.find(no_cursor_timeout=True)
 	for i, paper in enumerate(cursor):
@@ -130,12 +129,12 @@ def save_pkl(papers):
 	cursor.close()
 	Pickle.save_obj(ids, 'processed_data/ids')
 
-	# if os.path.exists('processed_data/citations.pkl'):
-		# citations = Pickle.load_obj('processed_data/citations')
-	# else:
+	# citation is list of id in ids and in either incitations or outcitations
+	print(type(ids))
 	citations = []
-	random.shuffle(ids)
+	citations_time = []
 	for i, id_ in enumerate(ids):
+		start_time = time.time()
 		paper = papers.find_one({'id': id_})
 		
 		a = [citation for citation in paper['incitations'] if citation not in citations and citation in ids]
@@ -143,13 +142,13 @@ def save_pkl(papers):
 		b = [citation for citation in paper['outcitations'] if citation not in citations and citation in ids]
 		citations += b
 
+
+		citations_time.append(time.time() - start_time)
+		print("citations time", time.time() - start_time)
 		print(i, len(citations))
-		# if len(citations) > 50000:
-			# break
-	ids = None
+
 
 	Pickle.save_obj(citations, 'processed_data/citations')
-	# print(len(list(set(citations))))
 
 	count = 0
 	cites = []
@@ -172,20 +171,141 @@ def save_pkl(papers):
 		MongoDB.insert_documents(database_name='OpenCorpus', collection_name='cite_map', doc=cite)
 		cites.append(cite)
 	Pickle.save_obj(cites, 'processed_data/cite_map')
-	return maps
+	return MongoDB.get_collection(database_name='OpenCorpus', collection_name='cite_map')
+
+
+def filter(cite_map):
+	final_cite_map = MongoDB.get_collection(database_name='OpenCorpus', collection_name='final_cite_map')
+	n_cite = final_cite_map.count()
+	if n_cite > 10000:
+		print("Data has been in DB, number of cite is: ", n_cite)
+		return final_cite_map
+	new_cite_map = MongoDB.get_collection(database_name='OpenCorpus', collection_name='new_cite_map')
+	n_cite = new_cite_map.count()
+	if n_cite > 10000:
+		print("Data has been in DB, number of cite is: ", n_cite)
+	cursor = cite_map.find(no_cursor_timeout=True)
+	if os.path.exists('processed_data/new_ids.pkl'):
+		new_ids = Pickle.load_obj('processed_data/new_ids')
+	else:
+		ids = []
+		for i, paper in enumerate(cursor):
+			ids.append(paper['id'])
+		cursor.close()
+		Pickle.save_obj(ids, 'processed_data/ids')
+		new_ids = []
+		count = 0
+		for i, id_ in enumerate(ids):
+			node = cite_map.find_one({'id': id_})
+			if len(node['incitations']) + len(node['outcitations']) > 1:
+				count += 1
+				print(count, i, len(ids))
+				new_ids.append(id_)
+		Pickle.save_obj(new_ids, 'processed_data/new_ids')
+
+	final_ids = new_ids
+	iterr = 0
+	for k in range(8):
+		iterr += 1
+		ids = []
+		count = 0
+		for i, id_ in enumerate(final_ids):
+			cite = cite_map.find_one({'id': id_})
+			start_time = time.time()
+			try:
+				incitations = cite['incitations']
+				filt_incitations = [incitations[i] for i in range(len(incitations)) if incitations[i] in final_ids]
+				
+				outcitations = cite['outcitations']
+				filt_outcitations = [outcitations[i] for i in range(len(outcitations)) if outcitations[i] in final_ids]
+			except Exception as err:
+				print(err)
+				continue
+
+			if len(filt_incitations) > 1 or len(filt_outcitations) > 1:
+				ids.append(id_)
+				cite = {'id': id_, 'incitations': filt_incitations, 'outcitations': filt_outcitations}
+				count += 1
+				print(count, i, len(final_ids), time.time() - start_time)
+				MongoDB.insert_documents(database_name='OpenCorpus', collection_name='new_cite_map' + str(iterr), doc=cite)
+
+		new_cite_map = MongoDB.get_collection(database_name='OpenCorpus', collection_name='new_cite_map' + str(iterr))
+		Pickle.save_obj(ids, 'processed_data/final_ids')
+		count = 0
+		final_ids = []
+		for i, id_ in enumerate(ids):
+			cite = new_cite_map.find_one({'id': id_})
+			start_time = time.time()
+			try:
+				incitations = cite['incitations']
+				filt_incitations = [incitations[i] for i in range(len(incitations)) if incitations[i] in ids]
+				
+				outcitations = cite['outcitations']
+				filt_outcitations = [outcitations[i] for i in range(len(outcitations)) if outcitations[i] in ids]
+			except Exception as err:
+				print(err)
+				continue
+
+			if len(filt_incitations) > 0 or len(filt_outcitations) > 0:
+				final_ids.append(id_)
+				cite = {'id': id_, 'incitations': filt_incitations, 'outcitations': filt_outcitations}
+				count += 1
+				print(count, i, len(ids), time.time() - start_time)
+				MongoDB.insert_documents(database_name='OpenCorpus', collection_name='final_cite_map' + '2' + str(iterr), doc=cite)
+		Pickle.save_obj(final_ids, 'processed_data/final_ids')
+		cite_map = MongoDB.get_collection(database_name='OpenCorpus', collection_name='final_cite_map' + '2' + str(iterr))
+
+def collect_paper(final_cite_map):
+	ids = Pickle.load_obj('processed_data/final_ids')
+	citations = []
+	for i, id_ in enumerate(ids):
+		cite = final_cite_map.find_one({'id': id_})
+		citations.append({'incitations': cite['incitations'], 'outcitations': cite['outcitations']})
+		print(i, len(citations[i]['incitations']), len(citations[i]['outcitations']))
+	papers = MongoDB.get_collection(database_name='OpenCorpus', collection_name='papers')
+	for i, id_ in enumerate(ids):
+		paper_info = papers.find_one({'id': id_})
+		needed_info = {
+					"id": paper_info['id'],						# "4cd223df721b722b1c40689caa52932a41fcc223"
+					"title": paper_info['title'],				# "Knowledge-rich, computer-assisted composition of Chinese couplets"
+					"abstract": paper_info['abstract'],	# "Recent research effort in poem composition has focused on the use of automatic language generation..."
+					"entities": paper_info['entities'],			# ["Conformance testing","Natural language generation"]
+					"authors": paper_info['authors'],			# [{"name": "Vinh", "ids":["123456"]},...{}...]
+					"incitations": citations[i]['incitations'],	# [ids..]
+					'outcitations': citations[i]['outcitations'],	# [ids...]
+					"year": paper_info['year']
+				}
+		print("adding new info to collection")
+		MongoDB.insert_documents(database_name='OpenCorpus', collection_name='final_papers', doc=needed_info)
+		a = np.random.rand(1)
+		if a < 0.1:
+			print(i, len(ids))
+
+		
 
 
 if __name__ == "__main__":
 	path = "raw_data"
-	# for ele in dirr:
-	dirr = os.listdir(path)	
-	while len(dirr) > 1:
-		for ele in dirr:
-			if not ele.endswith('.txt'):
-				new_path = path + '/' + ele
-				print(new_path)
-				papers = read_file(new_path)
-				os.remove(new_path)
-				break
-		dirr = os.listdir(path)
+	# load data to mongodb
+	papers = None
+	dirr = os.listdir(path)
+	# 00 - > 06
+	# while len(dirr) > 1:
+	# 	for ele in dirr:
+	# 		if not ele.endswith('.txt'):
+	# 			new_path = path + '/' + ele
+	# 			print(new_path)
+	# 			papers = read_file(new_path)
+	# 			os.remove(new_path)
+	# 			break
+	# 	dirr = os.listdir(path)
+	papers = read_file(path)
+	print("start mapping")
+	maps = save_pkl(papers)
+	final_cite_map = filter(maps)
+	# ids = Pickle.load_obj('processed_data/final_ids')
+	# print(len(ids))
+	collect_paper(final_cite_map)
+
+
 
---
+++ lda.py
@@ -0,0 +1,4 @@
+from gensim.test.utils import common_corpus
+from gensim.model
+# print(common_corpus)
+# common_corpus is a list of 10 very short document
